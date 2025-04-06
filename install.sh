b7e04efca3dcee1dccf47a59237c1d115a168c33
Tarcisio Miranda <tarcisio.miranda@valid.com>
2025-04-06 11:21:35 -0300

    update

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 installCursor() {
-    if ! [ -f /opt/cursor.appimage ]; then
-        echo "Installing Cursor AI IDE..."
-        APPIMAGE_PATH="/opt/cursor.appimage"
-        ICON_PATH="/opt/cursor.png"
-        DESKTOP_ENTRY_PATH="/usr/share/applications/cursor.desktop"
+  if ! [ -f /home/tarcisio/.local/bin/cursor.appimage ]; then
+    echo "Installing Cursor AI IDE..."
+    APPIMAGE_PATH="/home/tarcisio/.local/bin/cursor.appimage"
+    ICON_PATH="/home/tarcisio/.local/bin/cursor.png"
+    DESKTOP_ENTRY_PATH="/home/tarcisio/.local/share/applications/cursor.desktop"
 
-        echo "Copy Cursor AppImage..."
-        sudo cp Cursor.AppImage $APPIMAGE_PATH
-        sudo chmod +x $APPIMAGE_PATH
+    echo "Copy Cursor AppImage..."
+    sudo cp Cursor-0.48.7-x86_64.AppImage $APPIMAGE_PATH
+    sudo chmod +x $APPIMAGE_PATH
 
-        echo "Downloading Cursor icon..."
-        sudo cp icon.png $ICON_PATH
+    echo "Downloading Cursor icon..."
+    sudo cp icon.png $ICON_PATH
 
-        echo "Creating .desktop entry for Cursor..."
-        sudo bash -c "cat > $DESKTOP_ENTRY_PATH" <<EOL
+    echo "Creating .desktop entry for Cursor..."
+    sudo bash -c "cat > $DESKTOP_ENTRY_PATH" <<EOL
 [Desktop Entry]
 Name=Cursor AI
 Exec=$APPIMAGE_PATH --no-sandbox
@@ -22,10 +22,10 @@ Type=Application
 Categories=Development;
 EOL
 
-        echo "Cursor AI IDE installation complete. You can find it in your application menu."
-    else
-        echo "Cursor AI IDE is already installed."
-    fi
+    echo "Cursor AI IDE installation complete. You can find it in your application menu."
+  else
+    echo "Cursor AI IDE is already installed."
+  fi
 }
 
 installCursor
