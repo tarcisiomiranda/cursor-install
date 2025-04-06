352be6146324cd4a6d6c348bca52dec77d00e991
Tarcisio Miranda <tarcisio.miranda@valid.com>
2025-04-06 11:26:59 -0300

    update script

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 installCursor() {
-  if ! [ -f /home/tarcisio/.local/bin/cursor.appimage ]; then
+  if ! [ -f "$HOME/.local/bin/cursor.appimage" ]; then
     echo "Installing Cursor AI IDE..."
-    APPIMAGE_PATH="/home/tarcisio/.local/bin/cursor.appimage"
-    ICON_PATH="/home/tarcisio/.local/bin/cursor.png"
-    DESKTOP_ENTRY_PATH="/home/tarcisio/.local/share/applications/cursor.desktop"
+    APPIMAGE_PATH="$HOME/.local/bin/cursor.appimage"
+    ICON_PATH="$HOME/.local/bin/cursor.png"
+    DESKTOP_ENTRY_PATH="$HOME/.local/share/applications/cursor.desktop"
 
-    echo "Copy Cursor AppImage..."
-    sudo cp Cursor-0.48.7-x86_64.AppImage $APPIMAGE_PATH
-    sudo chmod +x $APPIMAGE_PATH
+    echo "Downloading Cursor AppImage..."
+    curl -L -o "$APPIMAGE_PATH" "https://downloads.cursor.com/production/1d623c4cc1d3bb6e0fe4f1d5434b47b958b05876/linux/arm64/Cursor-0.48.7-aarch64.AppImage"
+    chmod +x "$APPIMAGE_PATH"
 
     echo "Downloading Cursor icon..."
-    sudo cp icon.png $ICON_PATH
+    cp icon.png "$ICON_PATH"
 
     echo "Creating .desktop entry for Cursor..."
-    sudo bash -c "cat > $DESKTOP_ENTRY_PATH" <<EOL
+    cat > "$DESKTOP_ENTRY_PATH" <<EOL
 [Desktop Entry]
 Name=Cursor AI
 Exec=$APPIMAGE_PATH --no-sandbox
